b73c9fd373e18f19bbe7448e224e5065a5223c6f
Mustafa Ali KILCI <mustafa_klc997@hotmail.com>
2022-12-20 17:28:24 +0300

    Initial Commit

---
+++ CryptoSUI/CryptoSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSUIApp.swift
+//  CryptoSUI
+//
+//  Created by Mustafa Ali KILCI on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
